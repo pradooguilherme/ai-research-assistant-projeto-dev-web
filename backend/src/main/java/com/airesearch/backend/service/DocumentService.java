b8adf59be8b42e62eefb16d00b32e8759cf0e4ff
pradooguilherme <guipradu2020@gmail.com>
2026-03-28 02:04:47 -0300

    feat(service): add method to retrieve first 100 documents.

--- backend/src/main/java/com/airesearch/backend/service/DocumentService.java
+++ backend/src/main/java/com/airesearch/backend/service/DocumentService.java
@@ -4,6 +4,7 @@ import com.airesearch.backend.model.Document;
 
 public interface DocumentService {
     Iterable<Document> findAll();
+    Iterable<Document> findFirst100Documents();
     Document findById(Long id);
     void insert(Document document);
     void update(Long id, Document document);
