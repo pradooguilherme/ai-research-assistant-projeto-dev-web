5ed05e204e037d8e50b0dacf54fcabee943e7ea0
pradooguilherme <guipradu2020@gmail.com>
2026-03-28 02:03:06 -0300

    feat(repository): add query to fetch first 100 documents ordered by title.

--- backend/src/main/java/com/airesearch/backend/repository/DocumentRepository.java
+++ backend/src/main/java/com/airesearch/backend/repository/DocumentRepository.java
@@ -2,8 +2,16 @@ package com.airesearch.backend.repository;
 
 import com.airesearch.backend.model.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
 
 @Repository
 public interface DocumentRepository extends JpaRepository<Document, Long> {
+    @Query(
+            value =  "SELECT * FROM Document ORDER BY title LIMIT 100",
+            nativeQuery = true
+    )
+    Collection<Document> findFirst100Documents();
 }
