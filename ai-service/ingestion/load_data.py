a071f16fc7007fa2f07f73c2faa4f4a66bfa69ce
pradooguilherme <guipradu2020@gmail.com>
2026-04-03 00:32:19 -0300

    refactor(ingestion): centralize dataset loading utilities and standardize data paths

--- ai-service/ingestion/load_data.py
+++ ai-service/ingestion/load_data.py
@@ -1,15 +1,25 @@
 import pandas as pd
 
-PATH_TO_RAW_DATASET = "../data/raw/arXiv_scientific_dataset.csv"
+RAW_DATASET_PATH = "../data/raw/arXiv_scientific_dataset.csv"
 PREPROCESSED_PATH = "../data/raw/arXiv_scientific_dataset_loaded.csv"
+CLEANED_DATASET_PATH = "../data/processed/cleaned_documents.csv"
 
-# Load Data
+
 def load_raw_data() -> pd.DataFrame:
-    df = pd.read_csv(PATH_TO_RAW_DATASET)
-    print(f"Dataset loaded from {PATH_TO_RAW_DATASET}, shape {df.shape}")
+    df = pd.read_csv(RAW_DATASET_PATH)
+    print(f"Dataset loaded from {RAW_DATASET_PATH}, shape {df.shape}")
     return df
 
-# Show data
+def load_processed_data() -> pd.DataFrame:
+    df = pd.read_csv(PREPROCESSED_PATH)
+    print(f"Dataset loaded from {PREPROCESSED_PATH}, shape {df.shape}")
+    return df
+
+def load_cleaned_data() -> pd.DataFrame:
+    df = pd.read_csv(CLEANED_DATASET_PATH)
+    print(f"Dataset loaded from {CLEANED_DATASET_PATH}, shape {df.shape}")
+    return df
+
 def show_data(df: pd.DataFrame):
     print(f"Dataset shape: {df.shape}")
     print("--------------")
@@ -22,19 +32,18 @@ def show_data(df: pd.DataFrame):
     print(f"Dataset data examples:")
     print(df.head())
 
-
 def drop_duplicates(df: pd.DataFrame):
     df.drop_duplicates(inplace=True)
     df.drop_duplicates(subset=["summary", "title"], inplace=True)
     df.drop_duplicates(subset=["summary"], inplace=True)
     df.reset_index(drop=True, inplace=True)
 
-def save_processed_data(df: pd.DataFrame, path: str):
+def save_data(df: pd.DataFrame, path: str):
     df.to_csv(path, index=False)
-    print(f"Dataset processed saved to {path}")
+    print(f"Dataset saved to {path}")
 
 def main():
     df = load_raw_data()
     drop_duplicates(df)
     show_data(df)
-    save_processed_data(df, PREPROCESSED_PATH)
+    save_data(df, PREPROCESSED_PATH)
